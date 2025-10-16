87830351d1a6130c9d083365c665eaf8a03445eb
shintanr <shintanurrohmah143@gmail.com>
2025-10-16 13:29:08 +0700

    update apalah

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 // bikin pool (bukan connection tunggal)
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 // tes koneksi
